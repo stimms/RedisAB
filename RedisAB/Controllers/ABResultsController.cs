7ccc97586555c0445f014d1b777ef401e9d32fc9
Simon Timms <stimms@gmail.com>
2014-01-29 23:19:35 -0700

    Adding first step for retreiving keys for reports

---
+++ RedisAB/Controllers/ABResultsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookSleeve;
+using System.Threading.Tasks;
+
+namespace RedisAB.Controllers
+{
+    public class ABResultsController : Controller
+    {
+        //
+        // GET: /ABResults/
+        public async Task<ActionResult> Index()
+        {
+            using (var conn = new RedisConnection("localhost"))
+            {
+                await conn.Open();
+                return View(await conn.Keys.Find(0, "*"));
+            }
+            
+        }
+    }
+}
