7336b9b01010f6cdd9f4eb35d91dd1b4caaf85de
Simon Timms <stimms@gmail.com>
2014-02-07 15:59:07 -0700

    Finishing up admin interface

--- RedisAB/Controllers/ABResultsController.cs
+++ RedisAB/Controllers/ABResultsController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using BookSleeve;
 using System.Threading.Tasks;
+using System.Dynamic;
+using RedisAB.Models;
 
 namespace RedisAB.Controllers
 {
@@ -20,6 +22,28 @@ namespace RedisAB.Controllers
                 return View(await conn.Keys.Find(0, "*"));
             }
             
+        }
+
+        public async Task<ActionResult> Details(string keyName)
+        {
+            var campaignResults = new List<CampaignResult>();
+            using (var conn = new RedisConnection("localhost"))
+            {
+                await conn.Open();
+
+                foreach (var key in (await conn.Keys.Find(0, keyName + ".*")).Select(x=>x.Split('.').Skip(1).First()).Distinct())
+                {
+                    campaignResults.Add(new CampaignResult
+                    {
+                        Name = key,
+                        Successes = Int32.Parse(await conn.Strings.GetString(0, String.Format("{0}.{1}.success", keyName, key))),
+                        Total = Int32.Parse(await conn.Strings.GetString(0, String.Format("{0}.{1}", keyName, key)))
+                    });
+                }
+
+
+                return View(campaignResults);
+            }
         }
     }
 }
